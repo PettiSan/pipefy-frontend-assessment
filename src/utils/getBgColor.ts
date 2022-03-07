ab9c22371091757f2fd7fe36a4a536abc9666da7
Filipe Petitemberte <filipe.petitemberte@base.digital>
2022-03-07 14:57:05 -0300

    refactor: getBgColor typescript way

--- src/utils/getBgColor.ts
+++ src/utils/getBgColor.ts
@@ -1,31 +1,15 @@
-// TODO: Typescript way...
+const Colors: Record<string, string> = {
+  blue: "bg-blue-100",
+  red: "bg-red-100",
+  cyan: "bg-cyan-100",
+  orange: "bg-orange-100",
+  green: "bg-green-100",
+  pink: "bg-pink-100",
+  gray: "bg-gray-100",
+  purple: "bg-purple-100",
+  white: "bg-white",
+};
+
 export function getBgColor(color: string | null) {
-  switch (color) {
-    case "blue":
-      return "bg-blue-100";
-
-    case "red":
-      return "bg-red-100";
-
-    case "cyan":
-      return "bg-cyan-100";
-
-    case "orange":
-      return "bg-orange-100";
-
-    case "green":
-      return "bg-green-100";
-
-    case "pink":
-      return "bg-pink-100";
-
-    case "gray":
-      return "bg-gray-100";
-
-    case "purple":
-      return "bg-purple-100";
-
-    default:
-      return "bg-white";
-  }
+  return Colors[color || "white"];
 }
